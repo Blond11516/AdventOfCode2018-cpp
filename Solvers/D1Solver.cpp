a84640056aea23596b6b374a1d974cb11f45756c
Étienne Lévesque <etienne.levesque16@gmail.com>
2019-05-01 17:48:34 -0400

    Day 1, Challenge 2

--- Solvers/D1Solver.cpp
+++ Solvers/D1Solver.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <iostream>
+#include <set>
 #include "D1Solver.h"
 
 void D1Solver::solve1(std::list<D1Data> datum) {
@@ -15,5 +16,18 @@ void D1Solver::solve1(std::list<D1Data> datum) {
 }
 
 void D1Solver::solve2(std::list<D1Data> datum) {
+	std::set<int> frequencies = std::set<int>();
+	int sum = 0;
+    auto iter = datum.begin();
+	while (frequencies.find(sum) == frequencies.end()) {
+		frequencies.insert(sum);
 
+		sum += iter->getValue();
+		++iter;
+		if (iter == datum.end()) {
+			iter = datum.begin();
+		}
+	}
+
+	std::cout << sum;
 }
